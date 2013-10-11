fa3ac63820aacbc875c61e983429bfc30e95d866
river418 <zhouyicheng87+github@gmail.com>
2013-10-11 22:11:23 +0800

    add comment abount helloworldplugin.js

--- assets/www/helloworldplugin.js
+++ assets/www/helloworldplugin.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 var HelloWorldPlugin = {
-   createEvent:function(name,successCallback,errorCallback){
-	   cordova.exec(successCallback,
-	   errorCallback,
-	   'HelloWorldPlugin',
-	   'hello',
-	   [name]);
+   createEvent:function(name,successCallback,errorCallback){//name，成功回调，失败回调
+	   cordova.exec(successCallback,//成功回调方法
+	   errorCallback,//失败回调防范
+	   'HelloWorldPlugin',//Java端的插件名称
+	   'hello',//Action名称
+	   [name]);//传递给java端的参数
    }
    }
